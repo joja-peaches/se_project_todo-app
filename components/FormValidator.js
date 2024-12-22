4bdeefcc5c83c895f629cb242722de12f47a30ca
unknown <joja.peaches7@gmail.com>
2024-12-22 14:51:38 -0600

    Refactor form validation messages and button state

---
+++ components/FormValidator.js
@@ -0,0 +1,84 @@
+class FormValidator {
+    constructor(settings, formEl) {
+        this._inputSelector = settings.inputSelector;
+        this._submitButtonSelector = settings.submitButtonSelector;
+        this._errorClass = settings.errorClass;
+        this._inputErrorClass = settings.inputErrorClass;
+        this._inactiveButtonClass = settings.inactiveButtonClass;
+        this._formEl = formEl;
+    }
+
+_showInputError(formElement, inputElement, errorMessage) {
+  const errorElementId = `#${inputElement.id}-error`;
+  const errorElement = formElement.querySelector(errorElementId);
+  inputElement.classList.add(this._inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(this._errorClass);
+};
+
+_hideInputError(formElement, inputElement) {
+  const errorElementId = `#${inputElement.id}-error`;
+  const errorElement = formElement.querySelector(errorElementId);
+  inputElement.classList.remove(this._inputErrorClass);
+  errorElement.classList.remove(this._errorClass);
+  errorElement.textContent = "";
+};
+    
+_checkInputValidity(inputElement) {
+  if (!inputElement.validity.valid) {
+    this._showInputError(
+      this._formEl,
+      inputElement,
+      inputElement.validationMessage
+    );
+  console.log("not valid")
+  } else {
+    this._hideInputError(this._formEl, inputElement);
+  console.log("valid")
+  }
+};
+
+_hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+_toggleButtonState(inputList, buttonElement) {
+  if (this._hasInvalidInput(inputList)) {
+    buttonElement.classList.add(this._inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(this._inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+};
+
+_setEventListeners() {
+    this._inputList = Array.from(
+        this._formEl.querySelectorAll(this._inputSelector),
+      );
+        this._buttonElement = this._formEl.querySelector(
+        this._submitButtonSelector,
+      );
+
+      this._toggleButtonState(this._inputList, this._buttonElement);
+    
+      this._inputList.forEach((inputElement) => {
+        inputElement.addEventListener("input", () => {
+          this._checkInputValidity(inputElement);
+          this._toggleButtonState(this._inputList, this._buttonElement);
+        });
+      });
+  console.log(this._buttonElement);
+}
+
+enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+      });
+    this._setEventListeners();
+  }
+}
+
+export default FormValidator;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+// import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import FormValidator from '../components/FormValidator.js';
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-// const todoTemplate = document.querySelector("#todo-template"); -- remove
 const todosList = document.querySelector(".todos__list");
 
 const openModal = (modal) => {
@@ -35,11 +36,11 @@ addTodoForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
+  // const id = uuidv4();
 
-  // Create a date object and adjust for timezone
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
+  // const values = { name, date, id };
   const values = { name, date };
   const todo = generateTodo(values);
   todosList.append(todo);
@@ -50,3 +51,7 @@ initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
+
+newTodoValidator.enableValidation();
